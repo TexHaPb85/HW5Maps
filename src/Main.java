81c43847b318fa7ee079d923e3d47cc867e160ff
TexHaPb85 <16fi.m.yevtushenko@std.npu.edu.ua>
2019-07-04 13:14:10 +0300

    master branch is empty

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import library.Demonstrator;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Demonstrator demo = new Demonstrator();
-        demo.libaryWorkDemo();
-    }
-}
