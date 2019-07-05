b91d5f5806f462f442689512d501630f068bf5e0
TexHaPb85 <16fi.m.yevtushenko@std.npu.edu.ua>
2019-07-05 12:42:05 +0300

    support remarks fixed

---
+++ src/library/Demonstrator.java
@@ -0,0 +1,42 @@
+package library;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Demonstrator {
+
+    private static Library library = new Library();
+
+    static {
+        library.addUser(new LibraryUser("Mike"));
+
+        library.getUserByName("Mike").takeBook(new Date(99, 6, 5), "Gamlet");
+        library.getUserByName("Mike").takeBook(new Date(99, 6, 5), "Harry Potter");
+        library.getUserByName("Mike").takeBook(new Date(99, 6, 6), "The Song of ice and fire");
+        library.getUserByName("Mike").takeBook(new Date(99, 6, 7), "The Lord of the Ring");
+    }
+
+    public static void libraryWorkDemo() {
+        String ifContinue;
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.println(library.getUserByName("Mike").getBookByDate(enterDate(sc)) +
+                    "\nDo you want to check one more date(yes/no)");
+            ifContinue = sc.next();
+        } while (ifContinue.equals("yes"));
+
+
+        System.out.println("All the taken books:\n" + library.getUserByName("Mike").getAllBooks());
+        System.out.println("All the dates of book taking:\n" + library.getUserByName("Mike").getAllDates());
+    }
+
+    public static Date enterDate(Scanner sc) {
+        System.out.println("Enter day");
+        int day = sc.nextInt();
+        System.out.println("Enter month");
+        int month = sc.nextInt();
+        System.out.println("Enter year");
+        int year = sc.nextInt();
+        return new Date(year, month, day);
+    }
+}
---
+++ src/library/Library.java
@@ -0,0 +1,24 @@
+package library;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Library {
+    private List<LibraryUser> users;
+
+
+    public Library() {
+        this.users = new ArrayList<>();
+    }
+
+    public void addUser(LibraryUser user) {
+        users.add(user);
+    }
+
+    public LibraryUser getUserByName(String name) {
+        return users.stream().filter(user -> user.getName().equals(name)).findFirst().get();
+    }
+
+
+}
